249f9695c7e6cda7c8ea6d6154e3c76e224f31dc
onuralp <onuralparslan97@gmail.com>
2025-03-20 18:50:28 +0000

    update

--- checker.py
+++ checker.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-import os 
-import time 
-import subprocess
+import os
+import time
+import asyncio
 import protectless_version
 
-import asyncio
-asyncio.run(protectless_version.executeApp()) 
+async def main():
+    # Start the async function in the background
+    asyncio.create_task(protectless_version.executeApp())
 
+    while True:
+        os.system("git fetch")
+        results = os.popen("git status").read()
+        if "branch is behind" in results:
+            os.system("git pull")
+            print("gerisdesin")
+            await asyncio.sleep(5)  # Use async sleep
+            os.system("reboot")
+        else:
+            print("herikulade kod")
 
+        await asyncio.sleep(10)  # Use async sleep
 
-while True:
-    os.system("git fetch")
-    results=os.popen("git status").read()
-    if "branch is behind" in results:
-        os.system("git pull")
-        print("gerisdesin")
-        time.sleep(5)
-        os.system("reboot")        
-    else:
-        print("herikulade kod ")
-
-
-    time.sleep(10)
+asyncio.run(main())  # Run the event loop
