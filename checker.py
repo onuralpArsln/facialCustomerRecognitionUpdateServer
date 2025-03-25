55609e1b52070e11d5fe7e0acfd32aff0e3d8486
onuralp <onuralparslan97@gmail.com>
2025-03-25 10:44:46 +0000

    crontab açıklaması

--- checker.py
+++ checker.py
@@ -26,3 +26,15 @@ if __name__ == "__main__":
     process = multiprocessing.Process(target=checkUpdate)
     process.start()
     protectless_version.executeApp()
+
+
+### Crontablamak için 
+"""
+@reboot export DISPLAY=:0 &&  cd /home/proje/adresi && . .venv/bin/activate && python3 checjer.py >> /home/usr_name/cronlog.txt 2>&1 
+
+export DISPLAY=:0 ile ekrana gui açtır 
+cd ile pathte çalış
+. .venv/  source yerine . atmak daha güvenilir 
+>> /home/usr_name/cronlog.txt 2>&1  log al
+
+"""
